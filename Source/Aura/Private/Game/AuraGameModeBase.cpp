d7732815a63c76b95a067f2a7bb851fdbaa9efc4
veeyuh <33084815+ahhdim@users.noreply.github.com>
2025-02-08 20:15:41 -0800

    Done with Section 4

---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Imo Games
+
+
+#include "Game/AuraGameModeBase.h"
+
---
+++ Source/Aura/Private/Interaction/TargetInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright Imo Games
+
+
+#include "Interaction/TargetInterface.h"
+
+// Add default functionality here for any ITargetInterface functions that are not pure virtual.
